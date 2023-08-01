6fbaa0fff1c0754abd1dce1a5558789514fdad90
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-01 18:16:23 -0300

    feat: adicionando o header da página principal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native';
+import Logo from "../../assets/logo.svg"
+import Stroke from "../../assets/stroke.svg"
+import Message from "../../assets/message.svg"
 
 export function Home() {
   return (
     <View style={styles.container}>
-        <View>
+        <View style={styles.header}>
+            <Logo/>
+            <View style={styles.headerOptions}>
+                <Stroke/>
+                <Message/>
+            </View>
         </View>
     </View>
   );
@@ -15,4 +23,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#000',
     alignItems: 'center',
   },
+  header : {
+    marginTop: 40,
+    paddingHorizontal: 10,
+    width: '100%',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: 56,
+    flexDirection: 'row'
+  },
+  headerOptions: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    gap: 10
+  }
 });
