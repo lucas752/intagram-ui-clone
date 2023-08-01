aa30eb32eb7378c7197692f9adbd3fe3df104ad3
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-08-01 19:01:07 -0300

    feat: adicionando a seção de stories

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,74 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, Image, StyleSheet, Text, View } from 'react-native';
 import Logo from "../../assets/logo.svg"
 import Stroke from "../../assets/stroke.svg"
 import Message from "../../assets/message.svg"
+import storieImage from "../../assets/stories.png"
+import storieImage2 from "../../assets/stories1.png"
+import storieImage3 from "../../assets/stories2.png"
+import storieImage4 from "../../assets/stories3.png"
+
+const data = [
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage,
+        username: 'lewis'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage2,
+        username: 'alexpalou'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage3,
+        username: 'oceanbymr'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage4,
+        username: 'f1'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage,
+        username: 'lewisha...'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage2,
+        username: 'alexpalou'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage3,
+        username: 'oceanbymr'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage4,
+        username: 'f1'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage,
+        username: 'lewisha...'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage2,
+        username: 'alexpalou'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage3,
+        username: 'oceanbymr'
+    },
+    {
+        id: Math.random().toString(36).substring(2,27),
+        photoURL: storieImage4,
+        username: 'f1'
+    },
+]
 
 export function Home() {
   return (
@@ -12,6 +79,17 @@ export function Home() {
                 <Stroke/>
                 <Message/>
             </View>
+        </View>
+        <View style={styles.stories}>
+            <FlatList horizontal={true} data={data} keyExtractor={item => item.id} renderItem={item => (
+                <View style={styles.storiesContainer}>
+                    <View style={styles.storiesCard} key={item.item.id}>
+                        <Image style={styles.storiesCardImage} source={item.item.photoURL}/>
+                    </View>
+                    <Text style={styles.storiesUsername}>{item.item.username}</Text>
+                </View>
+            )}
+            />
         </View>
     </View>
   );
@@ -36,5 +114,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     gap: 10
+  },
+  stories: {
+    width: '100%',
+    paddingLeft: 10,
+    //height: 115,
+    alignContent: 'center',
+    paddingVertical: 10
+  },
+  storiesCard: {
+    borderWidth: 2,
+    borderColor: "#ff0267",
+    borderRadius: 50,
+    marginRight: 14,
+    padding: 2,
+  },
+  storiesCardImage: {
+    width: 64,
+    height: 64,
+    borderRadius: 50
+  },
+  storiesUsername: {
+    color: '#FFF',
+    justifyContent: 'center',
+  },
+  storiesContainer: {
+    alignItems: 'center'
   }
 });
